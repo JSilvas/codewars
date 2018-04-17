9d08e1899ba73b698ea7085aec1b3f7cbadf152e
Jay Silvas <jayinlava@gmail.com>
2018-04-17 01:17:36 -0700

    Added working solution

--- SumOfOddCubedNumbers/solution.js
+++ SumOfOddCubedNumbers/solution.js
@@ -1,18 +1,17 @@
 function cubeOdd(arr) {
-  
   let cubes = [];
-  
-  arr.forEach(num => {
-    if (num % 2 === 0) {
-  // cube odds in Array
-      let cube = Math.pow(num, 3);
-      arr.push(cube);
-    }
-  });  
-  
+  if (arr.some(isNaN)) {
+    return;
+  } else {
+    arr.forEach(n => {
+      n % 2 !== 0 ? cubes.push(Math.pow(n, 3)): false;
+    });
+  }
   return cubes.reduce((acc, cur) => acc + cur, 0);
 }
 
 
-console.log(cubeOdd([1, 2, 3, 4]));
-console.log(cubeOdd([1, 2, false, 3, 4]));
+console.log(cubeOdd([1, 2, 3, 4])); // 28
+console.log(cubeOdd([1, 2, false, 3, 4])); // 28
+console.log(cubeOdd(['h','g', 1, 2, 3, 4])); // undefined
+console.log(cubeOdd([[ 9, -7, -3, 23, 'a', -5, 29, 5 ]])); // undefined
